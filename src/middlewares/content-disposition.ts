fe0fed0b0ae96ada7345728a91480208eb2a1b39
Artur <admin@deswolf.com>
2025-02-15 14:09:10 +0000

    fix type

--- src/middlewares/content-disposition.ts
+++ src/middlewares/content-disposition.ts
@@ -1,6 +1,6 @@
 import { type Response, type Request, type NextFunction } from "express";
 
-export const contentDispositionMiddleware = (downloadFileParam: string | undefined) => (req: Request, res: Response, _next: NextFunction) => {
+export const contentDispositionMiddleware = (downloadFileParam: string | undefined | null | false) => (req: Request, res: Response, _next: NextFunction) => {
   // maybe add a whitelist/blacklist per mime type etc
   const contentDispositionHeader = res.getHeader("Content-Disposition");
   const isDownloadAllowed = downloadFileParam ? req.query[downloadFileParam] === "1" || req.query[downloadFileParam] === "true" : false;
