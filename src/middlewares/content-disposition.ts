87ad00d5b587344c320123ddbdc9c9fb7c84b003
Artur <admin@deswolf.com>
2025-03-11 14:34:18 +0000

    fix bug where response "user-agent" header is used instead of request

--- src/middlewares/content-disposition.ts
+++ src/middlewares/content-disposition.ts
@@ -1,8 +1,9 @@
 import { type Response, type Request, type NextFunction } from "express";
 
-function getStringHeaders<T extends string>(res: Response, keys: T[]): Record<T, string> {
+function getStringHeaders<T extends string>(resOrReq: Response | Request, keys: T[]): Record<T, string> {
+  const isResponse = "getHeader" in resOrReq && typeof resOrReq.getHeader === "function";
   return keys.reduce((acc, key) => {
-    const value = res.getHeader(key);
+    const value = isResponse ? resOrReq.getHeader(key) : resOrReq.header(key);
     return {
       ...acc,
       [key]: typeof value === "string" ? value : ""
@@ -12,12 +13,13 @@ function getStringHeaders<T extends string>(res: Response, keys: T[]): Record<T,
 
 export const contentDispositionMiddleware = (downloadFileParam: string | undefined | null | false) => (req: Request, res: Response, _next: NextFunction) => {
   // TODO: maybe add a whitelist/blacklist per mime type etc (.avi for IOS needs to be attachment)
-  const headers = getStringHeaders(res, ["Content-Disposition", "Content-Type", "Content-Length", "User-Agent"]);
+  const headers = getStringHeaders(res, ["Content-Disposition", "Content-Type", "Content-Length"]);
   const isDownloadAllowed = downloadFileParam ? req.query[downloadFileParam] === "1" || req.query[downloadFileParam] === "true" : false;
   if (!res.headersSent && !isDownloadAllowed && headers["Content-Disposition"].startsWith("attachment")) {
     const suffix = headers["Content-Disposition"].split(";")[1];
     res.setHeader("Content-Disposition", "inline" + (suffix ? `;${suffix}` : ""));
   }
+
   const mime = headers["Content-Type"];
   // allows .mkv to be played inline directly in browser
   if (mime === "video/x-matroska" && headers["Content-Disposition"].includes(".mkv")) {
@@ -27,7 +29,8 @@ export const contentDispositionMiddleware = (downloadFileParam: string | undefin
     // in case Content-Length is stripped (e.g. by cloudflare compression)
     res.setHeader("X-Decompressed-Content-Length", headers["Content-Length"]);
     // don't compress .mp4 for iOS, as it breaks the ability to be played inline directly in browser (e.g. fixes cloudflare .mp4 not playing in iOS browsers)
-    if (["video/mp4"].includes(mime) && (headers["User-Agent"].includes("iPhone") || headers["User-Agent"].includes("iPad"))) {
+    const reqHeaders = getStringHeaders(req, ["User-Agent"]);
+    if (["video/mp4"].includes(mime) && (reqHeaders["User-Agent"].includes("iPhone") || reqHeaders["User-Agent"].includes("iPad"))) {
       res.setHeader("Content-Encoding", "identity");
     }
   }
